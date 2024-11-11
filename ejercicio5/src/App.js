1e4acb78f192f377383beb359b095c4abddcb7ac
Cande77afk <candelopez4454@gmail.com>
2024-11-11 20:41:42 -0300

    ejercicio5

---
+++ ejercicio5/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+export default function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+              setProducts(json)
+            })
+            .catch(error => {
+              console.error('Error fetching products:', error);
+            });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
